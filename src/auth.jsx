b4c5b519ce076fc81a85fc852f7d24438439e8f9
mariasusan14 <mariasusan1404@gmail.com>
2024-03-06 22:18:48 +0530

    authentication changes

--- src/auth.jsx
+++ src/auth.jsx
@@ -1,107 +1,140 @@
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { collection, addDoc } from '@firebase/firestore';
+import { collection, addDoc, getDocs, query, where } from '@firebase/firestore';
 import { auth, db } from '../config/firebase';
 import { useNavigate } from 'react-router-dom';
-import { FaUser, FaLock,FaEnvelope,FaCheck,FaUserTag } from 'react-icons/fa';
-import './auth.css'
+import { FaUser, FaLock, FaEnvelope, FaCheck, FaUserTag } from 'react-icons/fa';
+import './auth.css';
 
 const Auth = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [mode, setMode] = useState('login');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [userType, setUserType] = useState('customer'); // Initialize userType with a default value
+  const [mode, setMode] = useState('login');
+  const [error, setError] = useState(null);
 
-    const navigate = useNavigate();
-    const userRef = collection(db, 'user');
+  const navigate = useNavigate();
+  const userRef = collection(db, 'user');
 
-    const handleToggleMode = () => {
-        setMode((prevMode) => (prevMode === 'login' ? 'signup' : 'login'));
-    };
+  const handleToggleMode = () => {
+    setMode((prevMode) => (prevMode === 'login' ? 'signup' : 'login'));
+    setError(null); // Clear any previous errors when toggling mode
+  };
 
-    const submitUser = async () => {
-        try {
-            const userDocRef = await addDoc(userRef, { email, password });
-            await createUserWithEmailAndPassword(auth, email, password);
+  const checkExistingAccount = async () => {
+    const userQuery = query(userRef, where('email', '==', email));
+    const querySnapshot = await getDocs(userQuery);
 
-            console.log('User document created in Firestore:', userDocRef.id);
-            console.log('User signed up successfully!');
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    return !querySnapshot.empty;
+  };
 
-    const login = async () => {
-        try {
-            await signInWithEmailAndPassword(auth, email, password);
-            console.log('User logged in successfully!');
-            navigate(`/dashboard/${auth.currentUser.uid}`);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const validateSignUpFields = () => {
+    return email && password && confirmPassword === password;
+  };
 
-    return (
-        <div className="login-container">
-            <div className="auth-container">
-            <div>
-                {mode === 'signup' && (
-                    <label className="auth-label">
-                    <FaUser />
-                        <input className="auth-input" placeholder="full name..." />
-                    </label>
-                )}
-                <label className="auth-label">
-                <FaEnvelope />
-                    <input
-                        className="auth-input"
-                        placeholder="email..."
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </label>
-                <label className="auth-label">
-                <FaLock />
-                    <input
-                        className="auth-input"
-                        placeholder="password..."
-                        onChange={(e) => setPassword(e.target.value)}
-                        type="password"
-                    />
-                </label>
-                {mode === 'signup' && (
-                    <label className="auth-label">
-                    <FaLock />
-                        <input className="auth-input" placeholder="confirm password..." type="password" />
-                    </label>
-                )}
-                {mode === 'signup' && (
-                    <label className="auth-label">
-                    <FaUserTag />
-                        <select className="auth-input">
-                            <option value="customer">Customer</option>
-                            <option value="shop">Shop</option>
-                            <option value="collab">Collaborator</option>
-                        </select>
-                    </label>
-                )}
-            </div>
-            <div>
-                    <button className="auth-button " onClick={mode === 'login' ? login : submitUser}>
-                        {mode === 'login' ? 'Login' : 'Sign Up'}
-                    </button>
-            </div>
-            <div>
-            {mode === 'login' && (
-                <label className="auth-label">
-                    Create new account
-                    <input className="auth-checkbox" type="checkbox" onChange={handleToggleMode} />
-                </label>
-                )}
-                
-            </div>
-            </div>
-            
+  const submitUser = async () => {
+    try {
+      const existingAccount = await checkExistingAccount();
+
+      if (existingAccount) {
+        setError('Account already exists. Please log in or use a different email.');
+      } else if (!validateSignUpFields()) {
+        setError('Please fill in all fields and ensure passwords match.');
+      } else {
+        const userDocRef = await addDoc(userRef, { email, password, userType });
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        console.log('User document created in Firestore:', userDocRef.id);
+        console.log('User signed up successfully!');
+        navigate(`/dashboard/${auth.currentUser.uid}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log('User logged in successfully!');
+      navigate(`/dashboard/${auth.currentUser.uid}`);
+    } catch (error) {
+      setError('Invalid email or password. Please try again.'); // Set login error message
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="auth-container">
+        <div>
+          {mode === 'signup' && (
+            <label className="auth-label">
+              <FaUser />
+              <input className="auth-input" placeholder="full name..." />
+            </label>
+          )}
+          <label className="auth-label">
+            <FaEnvelope />
+            <input
+              className="auth-input"
+              placeholder="email..."
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label className="auth-label">
+            <FaLock />
+            <input
+              className="auth-input"
+              placeholder="password..."
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+            />
+          </label>
+          {mode === 'signup' && (
+            <label className="auth-label">
+              <FaLock />
+              <input
+                className="auth-input"
+                placeholder="confirm password..."
+                type="password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </label>
+          )}
+          {mode === 'signup' && (
+            <label className="auth-label">
+              <FaUserTag />
+              <select
+                className="auth-input"
+                onChange={(e) => setUserType(e.target.value)}
+              >
+                <option value="customer">Customer</option>
+                <option value="shop">Shop</option>
+                <option value="collab">Collaborator</option>
+              </select>
+            </label>
+          )}
         </div>
-    );
+        <div>
+          <button className="auth-button " onClick={mode === 'login' ? login : submitUser}>
+            {mode === 'login' ? 'Login' : 'Sign Up'}
+          </button>
+        </div>
+        <div>
+          {mode === 'login' && (
+            <label className="auth-label">
+              Create a new account
+              <input className="auth-checkbox" type="checkbox" onChange={handleToggleMode} />
+            </label>
+          )}
+
+          {error && <div className="error-message">{error}</div>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,8 +1,28 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../config/firebase';
 
-export const Dashboard =()=>{
-  return(
+export const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      // Sign out the user using Firebase authentication
+      await auth.signOut();
+
+      // Redirect to the login page or any other page after logout
+      navigate('/auth');
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  };
+
+  return (
     <div>
-    <h1>hello</h1>
+      <h1>Hello, Welcome to the Dashboard!</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
-}
+};
+
+
